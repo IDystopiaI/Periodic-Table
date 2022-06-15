59cc07f572861f72da3de6dc97027c6ac00f3558
IDystopiaI <dylan.london94@gmail.com>
2022-06-15 03:03:27 +0000

    feat: Added a function to element.sh that determines if supplied argument is atomic_number, symbol or name

--- element.sh
+++ element.sh
@@ -3,15 +3,38 @@
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 
+# This function identifies if input is atomic number, symbol or name
+PARSE_INPUT() {
+  if [[ $1 =~ ^[0-9]{1,2}$ ]]
+  then
+    echo "number: $1"
+  
+  else
+  # use wc function and get the character count
+  # if characters are <= 2 $1 = symbol else $1 = name
+    if [[ ${#1} -le 2 ]]
+    then
+    echo "Symbol: $1"
+
+    else
+    echo "Name: $1"
+    fi
+  fi
+}
+
 
 # needs to accept atomic number, symbol or name
 if [[ -z $1 ]]
 then
+# does this need to be any element, symbol or number or only element's name?
   echo "Please provide an element as as argument"
   read INPUT_ELEMENT
 
   ELEMENT_RESPONSE=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) WHERE LOWER(name) = LOWER('$INPUT_ELEMENT')")
   echo "$ELEMENT_RESPONSE"
+
+else
+echo "$($echo PARSE_INPUT $1)"
 
 fi
 #  You should create an element.sh file in your repo folder for the program I want you to make
