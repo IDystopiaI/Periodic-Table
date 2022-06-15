e20766a21e116236b1bf7ef5929b172dae5ef3e9
IDystopiaI <dylan.london94@gmail.com>
2022-06-15 02:25:54 +0000

    feat: ask for element if not supplied with one, handles case insensitive element name input

--- element.sh
+++ element.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
+
+
+# needs to accept atomic number, symbol or name
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as as argument"
+  read INPUT_ELEMENT
+
+  ELEMENT_RESPONSE=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) WHERE LOWER(name) = LOWER('$INPUT_ELEMENT')")
+  echo "$ELEMENT_RESPONSE"
+
+fi
 #  You should create an element.sh file in your repo folder for the program I want you to make
+
 # Your script (.sh) file should have executable permissions
+
 # If you run ./element.sh, it should output Please provide an element as an argument and finish running.
+
 # If you run ./element.sh 1, ./element.sh H, or ./element.sh Hydrogen, it should output The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
+
 # If you run ./element.sh script with another element as input, you should get the same output but with information associated with the given element.
+
 # If the argument input to your element.sh script doesn't exist as an atomic_number, symbol, or name in the database, the output should be I could not find that element in the database.
