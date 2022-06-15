d609db6d421048e95b7ada745cf8fcd7570341aa
IDystopiaI <dylan.london94@gmail.com>
2022-06-15 03:30:18 +0000

    feat: bash script now handles queries using atomic numbers and informs the user if an element can not be found in the table using its atomic number

--- element.sh
+++ element.sh
@@ -7,7 +7,20 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 PARSE_INPUT() {
   if [[ $1 =~ ^[0-9]{1,2}$ ]]
   then
-    echo "number: $1"
+    # echo "number: $1"
+    # select statement works, need to read into VARIABLES and then create sentence
+
+    # needs to handle element can not be found
+    echo $($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) WHERE atomic_number = $1 ") | while read Q_ATOMIC_NUM BAR Q_NAME BAR Q_SYMBOL BAR Q_TYPE BAR Q_MASS BAR Q_MELT_C BAR Q_BOIL_C
+    do
+      # dont forget to add $ to varaiable name
+      if ! [[ -z $Q_NAME ]]
+      then
+        echo "The element with atomic number $Q_ATOMIC_NUM is $Q_NAME ($Q_SYMBOL). It's a $Q_TYPE, with a mass of $Q_MASS amu. $Q_NAME has a melting point of $Q_MELT_C celsius and a boiling point of $Q_BOIL_C celsius."
+      else
+        echo "I could not find that element in the database."
+      fi
+    done
   
   else
   # use wc function and get the character count
@@ -30,6 +43,7 @@ then
   echo "Please provide an element as as argument"
   read INPUT_ELEMENT
 
+  # place input into PARSE_INPUT function
   ELEMENT_RESPONSE=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) WHERE LOWER(name) = LOWER('$INPUT_ELEMENT')")
   echo "$ELEMENT_RESPONSE"
 
